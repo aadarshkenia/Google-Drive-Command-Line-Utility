c88d8d064e58597360d59d3644e1e93c2b50c33b
Aadarsh Kenia <aadarsh.kenia@stonybrook.edu>
2015-10-23 06:16:42 -0400

    Initial Commit

---
+++ gdupload.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[]){
+	char * filename = (char *) malloc(1000);
+	printf("Enter a filename to upload: \n");
+	scanf("%s", filename);
+	//Get file corresponding to this name
+	
+}
